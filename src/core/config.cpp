bae3952d3cb0aeadf2a1884a5a49ee4e2c405e0a
Patrick Schlangen <patrick@schlangen.me>
2025-12-02 19:13:25 +0000

    Update copright yer

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -1,6 +1,6 @@
 /*
  * b1gMailServer
- * Copyright (c) 2002-2022
+ * Copyright (c) 2002-2025
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
